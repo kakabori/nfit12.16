758d1f743b2e9cdcc9a0026061f231ed52018211
Kiyo <kakabori@andrew.cmu.edu>
2013-08-13 00:57:11 -0400

    implementing Lorentzian mosaic spread Need to implement interpolation over mosaic convoluted structure factor. Haven't modified the header file yet.

--- src/modelcalculator.cpp
+++ src/modelcalculator.cpp
@@ -168,6 +168,7 @@ double ModelCalculator::beamConvolutedStrFct(double qx)
   return result;
 }
 
+
 /******************************************************************************
 Return the integrand for the integration that performs beam convolution
 ******************************************************************************/
@@ -178,6 +179,7 @@ double ModelCalculator::s_convIntegrand(double qx, void *params)
 	       * p->beamProfile(qx - p->currQx);
 }
 
+
 /******************************************************************************
 Return the beam profile in q-space. It is assumed to be Gaussian.
 ******************************************************************************/
@@ -186,10 +188,11 @@ double ModelCalculator::beamProfile(double qx)
 	return exp(-qx * qx / 2 / beamSigma / beamSigma) / sqrt(2 * PI) / beamSigma;
 }
 
+
 /****************************************************************************************
 Static function that returns the logarithmic value of the rotated structure factor
 at the input log(qx). (Note that it is base e, not base 10)
-It takes the absolute value of qx. This is to deal with qx = 0 point, where
+It uses the absolute value of qx. This is to deal with qx = 0 point, where
 floating point arithmetic could potentially yield a very slightly negative value that
 represents zero in doulbe precision floating point.
 ****************************************************************************************/
@@ -201,13 +204,14 @@ double ModelCalculator::s_rotatedWrapper(double logqx, void *ptr)
 	return log(p->rotated(tmpQx));
 }
 
-/****************************************************************************************
-This function performs the integration over the structure factor along qy direction
-at the input qx value
-The lower and upper integration limits depend on qx, so their values get retrieved
-by ModelCalculator::getLowerLimit() and ModelCalculator::getUpperLimit() functions
-The interpolant for the structure factor along qy must get recalculated for each qx
-****************************************************************************************/
+
+/******************************************************************************
+This function performs the integration over the structure factor along qy 
+direction at the input qx value
+The lower and upper integration limits depend on qx, so their values get 
+retrieved by ModelCalculator::getLowerLimit() and 
+ModelCalculator::getUpperLimit() functions
+*******************************************************************************/
 double ModelCalculator::rotated(double qx)
 {
 	currQx = qx;
@@ -222,26 +226,61 @@ double ModelCalculator::rotated(double qx)
 	return result;
 }
 
+
 /******************************************************************************
 This function returns the integrand for the qy integration, which is
-the interpolated value of the structure factor at given qx, qy, and qz
-The interpolation over the structure factor is done in (qr,qz) tuple
-This function calculates qr corresponding to the input qy and currQx
-This wrapper is necessary for GSL qag
+the interpolated value of the mosaic convolved structure factor at given qx, 
+qy, and qz.
+The interpolation over the structure factor is done in (qr,qz) tuple.
+Calculates qr corresponding to the input qy and currQx.
+This wrapper is necessary for GSL qag.
 ******************************************************************************/
 double ModelCalculator::s_qyIntegrandWrapper(double qy, void *ptr)
 {
 	ModelCalculator *p = (ModelCalculator *)ptr;
 	double qr = sqrt(p->currQx * p->currQx + qy * qy);
-	return exp( p->spStrFct.val(log(qr + SMALLNUM)) );
+	return p->convolveMosaic(qr);
 }
 
-/****************************************************************************************
-Static wrapper for structure factor along qr direction
-The input is log(qr), base e
-Returns log(S(qr,qz))
-This wrapper is necessary for FunSupport object
-****************************************************************************************/
+
+double ModelCalculator::convolveMosaic(double qr)
+{
+  currQr = qr;
+  double result, abserr;
+  gsl_function F;
+  F.function = &s_mosaicIntegrandWrapper;
+  F.params = this;
+  double lowerLimit = 0;
+  double upperLimit = ;
+  gsl_integration_qag(&F, lowerLimit, upperLimit, g_epsabs, g_epsrel,
+                      WORKSPACE_SIZE, KEY, workspace, &result, &abserr);
+  return result;
+}
+
+
+double ModelCalculator::s_mosaicIntegrandWrapper(double q, void *ptr)
+{
+  ModelCalculator *p = (ModelCalculator *)ptr;
+	return exp(p->spStrFct.val(log(q+SMALLNUM))) * p->mosaicDist(p->currQr-q);
+}
+
+
+/******************************************************************************
+Mosaic spread distribution. Mosaic angle is approximated by qr/qz, which is
+good for small angle.
+******************************************************************************/
+double ModelCalculator::mosaicDist(r)
+{
+  return mosaic / (r*r + mosaic*mosaic);
+}
+
+
+/******************************************************************************
+Static wrapper for structure factor along qr direction.
+The input is log(qr), base e.
+Returns log(S(qr,qz)).
+This wrapper is necessary for FunSupport object.
+******************************************************************************/
 double ModelCalculator::s_StrFctWrapper(double logqr, void *ptr)
 {
   ModelCalculator *p = (ModelCalculator *)ptr;
@@ -256,10 +295,11 @@ double ModelCalculator::s_StrFctWrapper(double logqr, void *ptr)
   return log(ret);
 }
 
-/****************************************************************************************
-This function calculates the structure factor as a function of qr for a fixed value
-of qz specified by qz variable
-****************************************************************************************/
+
+/******************************************************************************
+This function calculates the structure factor as a function of qr for a fixed 
+value of qz specified by qz variable
+******************************************************************************/
 double ModelCalculator::StrFct(double qr)
 {
   currQr = qr;
@@ -270,7 +310,7 @@ double ModelCalculator::StrFct(double qr)
   double lowerLimit = 0;
   double upperLimit = cutoff_r;
   gsl_integration_qag(&F, lowerLimit, upperLimit, g_epsabs, g_epsrel,
-                               WORKSPACE_SIZE, KEY, workspace, &result, &abserr);
+                      WORKSPACE_SIZE, KEY, workspace, &result, &abserr);
   return result;
 }
 
@@ -284,8 +324,8 @@ J_0(qr*r) is the zeroth order Bessel function of the first kind
 double ModelCalculator::s_Fr(double r, void *ptr)
 {
 	ModelCalculator *p = (ModelCalculator *)ptr;
-  return r * p->spHr.val(r) * bessel_J0(p->currQr * r)
-          * p->spsumn.val(log(r+SMALLNUM)) / ARB_SCALE;
+  return r * p->spHr.val(r) * bessel_J0(p->currQr*r) *
+         p->spsumn.val(log(r+SMALLNUM)) / ARB_SCALE;
 }
 
 /******************************************************************************
@@ -301,7 +341,7 @@ retrieved.
 double ModelCalculator::s_sumnWrapper(double logr, void *ptr)
 {
   ModelCalculator *p = (ModelCalculator *)ptr;
-  double tmpR = fabs( exp(logr) - SMALLNUM );
+  double tmpR = fabs(exp(logr) - SMALLNUM);
   return p->sumn(tmpR);
 }
 
@@ -314,24 +354,25 @@ double ModelCalculator::sumn(double r)
   double sum = 0;
   double un, t1, t2;
   double qz2 = qz * qz;
-  double qzsig2 = (qz * edisp) * (qz * edisp);
+  double qzsig2 = (qz*edisp) * (qz*edisp);
   double lambda = 1e16 * sqrt(Kc/B) / dspacing;
   double eta = 2.17 * (T+273.15) / sqrt(Kc*B) / dspacing /dspacing;
 
   //Calculate n=0 term separately because it has 1/2 weight relatively to n>0 terms
   int n = 0;
   un = utable.getHeightDiffFunc(n, r, lambda, eta, dspacing);
-  t1 = 1 + qzsig2 * un;
+  t1 = 1 + qzsig2*un;
   t2 = n * dspacing;
   sum += Hz(n) * sqrt(1/t1) * exp( -0.5*(qz2*un+t2*t2*qzsig2)/t1 ) * cos(t2*qz/t1);
   for(n = 1; n < cutoff_n; n++) {
     un = utable.getHeightDiffFunc(n, r, lambda, eta, dspacing);
-    t1 = 1 + qzsig2 * un;
+    t1 = 1 + qzsig2*un;
     t2 = n * dspacing;
     sum += 2 * Hz(n) * sqrt(1/t1) * exp( -0.5*(qz2*un+t2*t2*qzsig2)/t1 ) * cos(t2*qz/t1);
   }
   return sum;
 }
+
 
 /******************************************************************************
 Static function that wrapps ModelCalculator::Hr
@@ -342,11 +383,12 @@ double ModelCalculator::s_HrWrapper(double r, void *ptr)
   return p->Hr(r);
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 Effective finite size factor in r direction, H_r(r)
 The upper limit is chosen to be 10 times the cutoff in r integration.
 The factor of 10 is somewhat arbitrary.
-****************************************************************************************/
+******************************************************************************/
 double ModelCalculator::Hr(double r)
 {
   curr_r = r;
@@ -361,70 +403,77 @@ double ModelCalculator::Hr(double r)
   return PI * result;
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 The integrand in H_r(r) integration
-****************************************************************************************/
+******************************************************************************/
 double ModelCalculator::s_HrIntegrand(double Lr, void *params)
 {
   ModelCalculator *p = (ModelCalculator *)params;
-  return Pr(Lr, p->avgLr) * Lr * Lr * finiteSizeFactor(p->curr_r / Lr);
+  return Pr(Lr, p->avgLr) * Lr * Lr * finiteSizeFactor(p->curr_r/Lr);
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 In-plane domain size distribution
 Can be changed to another function easily
-****************************************************************************************/
+******************************************************************************/
 double Pr(double Lr, double avgLr)
 {
   return exp(-Lr / avgLr) / avgLr;
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 Finite size factor in r direction, F_r(r/Lr)
-****************************************************************************************/
+******************************************************************************/
 double finiteSizeFactor(double x)
 {
   if (x > 1) {
     return 0;
   } else {
-    return acos(x) - x * sqrt(1-x*x);
+    return acos(x) - x*sqrt(1-x*x);
   }
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 Effective finite size factor in z direction, H_z(nD)
-****************************************************************************************/
+******************************************************************************/
 double ModelCalculator::Hz(int n)
 {
   return exp(-n/avgMz);
 }
 
-/****************************************************************************************
-Return the lower limit of the qy integration for the current value of qx and qz
-It is derived in an appropirate report
-****************************************************************************************/
+
+/******************************************************************************
+Return the lower limit of the qy integration for the current value of qx and 
+qz. The equation is derived in an appropirate report.
+******************************************************************************/
 double getLowerLimit(double qx, double qz, double wavelength)
 {
-	double theta = 0.5 * asin(sqrt(qx * qx + qz * qz) * wavelength / 2 / PI);
-	return -sqrt((4 * PI * sin(theta) / wavelength) * (4 * PI * sin(theta) /wavelength)
-	                                                      - qx * qx - qz * qz);
+	double theta = 0.5 * asin(sqrt(qx*qx+qz*qz)*wavelength/2/PI);
+	return -sqrt((4*PI*sin(theta)/wavelength)*(4*PI*sin(theta)/wavelength) -
+	             qx*qx -qz qz);
 }
 
-/****************************************************************************************
-Return the upper limit of the qy integration for the currenct value of qx and qz
-It is derived in an appropriate report
-****************************************************************************************/
+
+/******************************************************************************
+Return the upper limit of the qy integration for the currenct value of qx and 
+qz. The equation is derived in an appropriate report.
+******************************************************************************/
 double getUpperLimit(double qx, double qz, double wavelength)
 {
-	double omega = 2 * asin(wavelength * qz / 4 / PI);
-	return getLowerLimit(qx, qz, wavelength) * cos(omega)
-                    * (1 + tan(omega) * tan(omega)) + qz * tan(omega);
+	double omega = 2 * asin(wavelength*qz/4/PI);
+	return getLowerLimit(qx, qz, wavelength)*cos(omega)*
+	       (1+tan(omega)*tan(omega)) + qz*tan(omega);
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 given a slice with qz = tqz, update necessary temporary variables
 and tables.
-****************************************************************************************/
+******************************************************************************/
 void ModelCalculator::setSliceParameter(double _qz)
 {
   qz = _qz;
@@ -439,6 +488,7 @@ void ModelCalculator::KcBDTChanged()
   //out = eta * dspacing * dspacing / utable.D / utable.D / utable.eta;
 }
 
+
 /******************************************************************************
 update domain size parameters
 When they are modified, the cutoffs and the interpolant must also be modified
@@ -449,10 +499,12 @@ void ModelCalculator::avgLrChanged()
   spHr.findPoints(0, cutoff_r);
 }
 
+
 void ModelCalculator::avgMzChanged()
 {
 	cutoff_n = 20 * avgMz;
 }
+
 
 /******************************************************************************
 Converts beam FWHM in pixels to beam Gaussian sigma in q-space units
@@ -463,6 +515,7 @@ void ModelCalculator::set_beamSigma()
 	// bFWHM / 2.3548 is equal to beam Gaussian sigma
 	beamSigma = deltaQPerPixel * bFWHM / 2.3548;
 }
+
 
 /******************************************************************************
 This function sets a model parameter to an input value.
@@ -479,10 +532,12 @@ void ModelCalculator::setModelParameter(double value, const char *_name)
 	setModelParameter(value, name);
 }
 
+
 void ModelCalculator::setModelParameter(double value, const string& name)
 {
   setpara(value, stringToVarEnum(name));
 }
+
 
 /******************************************************************************
 set parameters for Sccd calculation
@@ -510,9 +565,10 @@ void ModelCalculator::setpara(double a, int idx)
   }
 }
 
-/****************************************************************************************
+
+/******************************************************************************
 make the ModelCalculator in sync with parameter set p
-****************************************************************************************/
+******************************************************************************/
 void ModelCalculator::paraSet(Para *p)
 {
   Kc = p->Kc;
@@ -533,25 +589,30 @@ void ModelCalculator::paraSet(Para *p)
   set_beamSigma();
 }
 
+
 void ModelCalculator::get_spStrFctPoints(vector<double>& x, vector<double>& y)
 {
 	spStrFct.getPoints(x, y);
 }
+
 
 void ModelCalculator::get_spsumnPoints(vector<double>& x, vector<double>& y)
 {
 	spsumn.getPoints(x, y);
 }
 
+
 void ModelCalculator::get_spHrPoints(vector<double>& x, vector<double>& y)
 {
 	spHr.getPoints(x, y);
 }
 
+
 void ModelCalculator::get_spRotatedPoints(vector<double>& x,vector<double>& y)
 {
 	spRotated.getPoints(x, y);
 }
+
 
 void ModelCalculator::getRotatedSF(double qxlow, double qxhigh, double _qz, vector<double>& qxvec, vector<double>& rsf)
 {
@@ -564,6 +625,7 @@ void ModelCalculator::getRotatedSF(double qxlow, double qxhigh, double _qz, vect
 		qx += 0.001;
 	}
 }
+
 
 void ModelCalculator::getStrFct(double qrlow, double qrhigh, double _qz, vector<double>& qrvec, vector<double>& sf)
 {
@@ -579,6 +641,7 @@ void ModelCalculator::getStrFct(double qrlow, double qrhigh, double _qz, vector<
 	}
 }
 
+
 void ModelCalculator::getSumn(double rlow, double rhigh, double _qz, vector<double>& rvec, vector<double>& zvec)
 {
 	setSliceParameter(_qz);
@@ -589,6 +652,7 @@ void ModelCalculator::getSumn(double rlow, double rhigh, double _qz, vector<doub
 	}
 }
 
+
 void ModelCalculator::getHr(double rlow, double rhigh, vector<double>& rvec, vector<double>& hvec)
 {
 	avgLrChanged();
@@ -598,6 +662,7 @@ void ModelCalculator::getHr(double rlow, double rhigh, vector<double>& rvec, vec
 		r += 100;
 	}
 }
+
 
 void saveDoubleColumns(vector<double>& xvec, vector<double>& yvec, const char *filename)
 {
